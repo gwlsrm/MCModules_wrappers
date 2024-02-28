321561a99caeb0b2afc11860dd96521c2d10b712
gwlsrm <gwrass@mail.ru>
2024-02-28 19:58:00 +0300

    tccfcalc_wrapper: added multithread support

--- tccfcalc_wrapper/tccfcalc_test.py
+++ tccfcalc_wrapper/tccfcalc_test.py
@@ -25,32 +25,33 @@ effmaker_additional_geom_list = ['sphere', 'cuboid']
 nuclide_list = ['co-60', 'eu-152', 'k-40']
 
 
-def form_infile_name(det, geom, nuclide):
+def _form_filename_suffix(det, geom, nuclide, is_multithread=False):
+    tokens = [det, geom]
     if nuclide:
-        return f'in_files{os.sep}tccfcalc_{det}_{geom}_nuclide.in'
-    else:
-        return f'in_files{os.sep}tccfcalc_{det}_{geom}.in'
+        tokens.append(nuclide)
+    if is_multithread:
+        tokens.append('mt')
+    return '_'.join(tokens)
+
+
+def form_infile_name(det, geom, nuclide, is_multithread=False):
+    fname_suffix = _form_filename_suffix(det, geom, 'nuclide' if nuclide else '', is_multithread)
+    return f'in_files{os.sep}tccfcalc_{fname_suffix}.in'
 
 
 def form_infile_json_name(det, geom, nuclide):
-    if nuclide:
-        return f'in_files{os.sep}tccfcalc_input_{det}_{geom}_{nuclide}.json'
-    else:
-        return f'in_files{os.sep}tccfcalc_input_{det}_{geom}.json'
+    fname_suffix = _form_filename_suffix(det, geom, nuclide)
+    return f'in_files{os.sep}tccfcalc_input_{fname_suffix}.json'
 
 
 def form_outfile_name(det, geom, nuclide):
-    if nuclide:
-        return f'out_files{os.sep}tccfcalc_{det}_{geom}_{nuclide}.out'
-    else:
-        return f'out_files{os.sep}tccfcalc_{det}_{geom}.out'
+    fname_suffix = _form_filename_suffix(det, geom, nuclide)
+    return f'out_files{os.sep}tccfcalc_{fname_suffix}.out'
 
 
-def form_resfile_name(det, geom, nuclide):
-    if nuclide:
-        return f'results{os.sep}tccfcalc_{det}_{geom}_{nuclide}.out'
-    else:
-        return f'results{os.sep}tccfcalc_{det}_{geom}.out'
+def form_resfile_name(det, geom, nuclide, is_multithread=False):
+    fname_suffix = _form_filename_suffix(det, geom, nuclide, is_multithread)
+    return f'results{os.sep}tccfcalc_{fname_suffix}.out'
 
 
 def form_res_spectrum_name(det, geom, nuclide):
@@ -71,6 +72,8 @@ def main():
                         action="store_true", default=False)
     parser.add_argument('--not-calc-effmaker', help='do not run effmaker source efficiency calculation task',
                         action="store_true", default=False)
+    parser.add_argument('--not-calc-mt', help='do not run multithread tasks',
+                        action="store_true", default=False)
     parser.add_argument('--calc-addition-effmaker', help='run additional effmaker source efficiency calculation task',
                         action="store_true", default=False)
     parser.add_argument('--cmp-with-errors', help='using uncertainties to compare results',
@@ -81,6 +84,7 @@ def main():
     calc_coinc_task = not args.not_calc_coinc
     calc_json_task = not args.not_calc_json
     calc_effmaker_task = not args.not_calc_effmaker
+    calc_mt_task = not args.not_calc_mt
     calc_addition_effmaker = args.calc_addition_effmaker
     # logger
     logging.basicConfig(
@@ -129,6 +133,22 @@ def main():
             shutil.copy('tccfcalc.out', form_resfile_name(det, geom, nuclide))
             shutil.copy('test_spectr.spe', form_res_spectrum_name(det, geom, nuclide))
             shutil.copy('test_spectr_coi.spe', form_res_coincspectrum_name(det, geom, nuclide))
+
+    # multithread calculation
+    if calc_mt_task:
+        for det, geom, nuclide in tqdm([('hpge', 'cylinder', ''), ('scintil', 'point', 'co-60')]):
+            tccfcalc_name = form_infile_name(det, geom, nuclide, is_multithread=True)
+            outfname = form_outfile_name(det, geom, nuclide)
+            logging.info('calc with: ' + tccfcalc_name)
+            shutil.copy(tccfcalc_name, 'tccfcalc.in')
+            nucl = Nuclide.parse_from(nuclide) if nuclide else Nuclide.get_default()
+            calculate_eff(nucl, N, None, SEED, ACTIVITY)
+            res = compare_out_files('tccfcalc.out', outfname, rel_eps=None)
+            if not res:
+                logging.error(f'tccfcal.out != {outfname}')
+            else:
+                logging.info(f'tccfcal.out == {outfname}')
+            shutil.copy('tccfcalc.out', form_resfile_name(det, geom, nuclide, is_multithread=True))
 
     # tccfcalc json calculation cycle
     if calc_json_task:
