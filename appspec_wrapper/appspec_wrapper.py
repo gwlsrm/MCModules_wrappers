bbf84a92ec1380cce30ffd2f3a28ccfbbb7ebfee
gwqw <gwrass@mail.ru>
2024-07-17 19:25:48 +0300

    appspec: fix for efficiency calculation (still doesn't work)

--- appspec_wrapper/appspec_wrapper.py
+++ appspec_wrapper/appspec_wrapper.py
@@ -1,7 +1,7 @@
 import os.path
 import sys
 import typing as tp
-from ctypes import CDLL, RTLD_GLOBAL, POINTER, c_int, c_bool, c_double, c_char_p
+import ctypes as ct
 
 from read_output_bin import convert_from_bin_to_txt
 
@@ -31,7 +31,7 @@ class AppspecDllWrapper:
             path_to_dll = os.getcwd()
         if lib_name is None:
             lib_name = self._auto_select_lib_name(path_to_dll)
-        self._lib = CDLL(os.path.join(path_to_dll, lib_name), RTLD_GLOBAL)
+        self._lib = ct.CDLL(os.path.join(path_to_dll, lib_name), ct.RTLD_GLOBAL)
         # efficiency
         # prepare
         self._prepare_efficiency_calculation = _get_attribute(
@@ -39,12 +39,12 @@ class AppspecDllWrapper:
             ['prepare_efficiency_calculation@20', 'prepare_efficiency_calculation']
         )
         self._prepare_efficiency_calculation.argtypes = [
-            c_int, POINTER(c_double), POINTER(c_double), POINTER(c_double), c_bool]
+            ct.c_int, ct.POINTER(ct.c_double), ct.POINTER(ct.c_double), ct.POINTER(ct.c_double), ct.c_bool]
         # calculate
         self._calculate_efficiency = _get_attribute(self._lib, ['calculate_efficiency@40', 'calculate_efficiency'])
         self._calculate_efficiency.argtypes = [
-            c_double, c_double, c_double, c_double, POINTER(c_double), POINTER(c_double)]
-        self._calculate_efficiency.restype = c_double
+            ct.c_double, ct.c_double, ct.c_double, ct.c_double, ct.POINTER(ct.c_double), ct.POINTER(ct.c_double)]
+        self._calculate_efficiency.restype = ct.c_int
         # reset
         self._reset_efficiency_calculation = _get_attribute(
             self._lib,
@@ -53,7 +53,7 @@ class AppspecDllWrapper:
         # spectrum
         self._calc_apparatus_spectrum = _get_attribute(
             self._lib, ['calc_apparatus_spectrum@4', 'calc_apparatus_spectrum'])
-        self._calc_apparatus_spectrum.argtypes = [c_char_p]
+        self._calc_apparatus_spectrum.argtypes = [ct.c_char_p]
 
     @staticmethod
     def _auto_select_lib_name(path_to_dll: str):
@@ -62,18 +62,23 @@ class AppspecDllWrapper:
                 return lib_name
         raise AttributeError(f'cannot find appspec library in "{path_to_dll}"')
 
-    def prepare_efficiency_calculation(self, arrays_size: int, energy_array: tp.List[float],
+    def prepare_efficiency_calculation(self, energy_array: tp.List[float],
                                        nfep_array: tp.List[float], dfep_array: tp.List[float],
                                        is_log: bool) -> None:
-        return self._prepare_efficiency_calculation(arrays_size, energy_array, nfep_array, dfep_array, is_log)
+        e = (ct.c_double* len(energy_array))(*energy_array)
+        nfep = (ct.c_double* len(nfep_array))(*nfep_array)
+        dfep = (ct.c_double* len(dfep_array))(*dfep_array)
+
+        return self._prepare_efficiency_calculation(
+            len(energy_array), e, nfep, dfep, is_log)
 
     def calculate_efficiency(self, energy: float, peak_count_rate: float, dpeak_count_rate: float,
-                             peak_intensity: float, efficiency: float, defficiency: float
-                             ) -> tp.Tuple[tp.List[float], tp.List[float], int]:
-        efficiency = []
-        defficiency = []
-        error_num = self._calculate_efficiency(energy, peak_count_rate, dpeak_count_rate, peak_intensity,
-                                               efficiency, defficiency)
+                             peak_intensity: float) -> tp.Tuple[float, float, int]:
+        efficiency = ct.c_double(-1.0)
+        defficiency = ct.c_double(-1.0)
+        error_num = self._calculate_efficiency(
+            energy, peak_count_rate, dpeak_count_rate, peak_intensity,
+            ct.pointer(efficiency), ct.pointer(defficiency))
         return efficiency, defficiency, error_num
 
     def reset_efficiency_calculation(self) -> None:
