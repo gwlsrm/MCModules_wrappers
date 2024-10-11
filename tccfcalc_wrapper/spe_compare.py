c190a8c6e06959504303e11be063955db464c15b
gwqw <gwqw@mail.ru>
2024-10-11 20:02:41 +0300

    appspec: calculate_efficiency_json for windows tccfcalc: compare spectra approximately

--- tccfcalc_wrapper/spe_compare.py
+++ tccfcalc_wrapper/spe_compare.py
@@ -4,6 +4,8 @@ import numpy as np
 
 from utils.speparser import SpectrumReader
 
+EPSILON = 1
+
 
 def compare_spectra(txt_filename_1: str, txt_filename_2: str) -> bool:
     spe1 = SpectrumReader.parse_txt(txt_filename_1)
@@ -16,7 +18,12 @@ def compare_spectra(txt_filename_1: str, txt_filename_2: str) -> bool:
         logging.error(f"different live times: {spe1.info.treal} != {spe2.info.treal}")
         return False
 
-    data_res = np.all(spe1.data == spe2.data)
+    if len(spe1.data) != len(spe2.data):
+        logging.error("different spectrum lengths")
+        return False
+
+    cmp_res = np.abs(spe1.data - spe2.data) <= 2 * np.sqrt(spe1.data + spe2.data) + EPSILON
+    data_res = np.all(cmp_res)
     if not data_res:
         logging.error("different spectrum counts")
     return data_res
